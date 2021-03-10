d1f5b55f947146fe1406d2411c83dba386915950
E-Burton <edwina.antoinette@gmail.com>
2021-03-09 20:15:31 -0700

    Updating app with express, service worker, and licenses; Updated README

---
+++ client/src/views/MantraPage/MantraPage.js
@@ -0,0 +1,176 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Slide from "@material-ui/core/Slide";
+import IconButton from "@material-ui/core/IconButton";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+
+// @material-ui/icons
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import Close from "@material-ui/icons/Close";
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import SpaIcon from '@material-ui/icons/Spa';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+
+// core components
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+import Button from "components/CustomButtons/Button.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import NavPills from "components/NavPills/NavPills.js";
+import Parallax from "components/Parallax/Parallax.js";
+import Intermediate from "views/MantraPage/Sections/Intermediate.js";
+
+import profile from "assets/img/buddha2.jpg";
+
+import studio1 from "assets/img/examples/studio-1.jpg";
+import studio3 from "assets/img/examples/studio-3.jpg";
+
+import work1 from "assets/img/examples/olu-eletu.jpg";
+import work2 from "assets/img/examples/clem-onojeghuo.jpg";
+import work4 from "assets/img/examples/mariya-georgieva.jpg";
+
+import styles from "assets/jss/material-kit-react/views/profilePage.js";
+import javaStyles from "assets/jss/material-kit-react/views/componentsSections/javascriptStyles.js";
+
+const useStyles = makeStyles(styles);
+
+// const imgStyle = {
+//     width: "100%",
+//     height: "auto",
+//     aspectRatio: Attr(100)/Attr("auto")
+// };
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
+
+Transition.displayName = "Transition";
+
+export default function MantraPage(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  const imageClasses = classNames(
+    classes.imgRaised,
+    classes.imgRoundedCircle,
+    classes.imgFluid
+  );
+  const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
+  const [classicModal, setClassicModal] = React.useState(false);
+  return (
+    <div>
+      <Header
+        color="transparent"
+        brand="World of Meditation"
+        rightLinks={<HeaderLinks />}
+        fixed
+        changeColorOnScroll={{
+          height: 200,
+          color: "white"
+        }}
+        {...rest}
+      />
+      <Parallax small filter image={require("assets/img/bg12.jpg")} />
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div>
+          <div className={classes.container}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={6}>
+                <div className={classes.profile}>
+                  <div>
+                    <img src={profile} alt="..." className={imageClasses} />
+                  </div>
+                  <div className={classes.name}>
+                    <h3 className={classes.title}>Mantra Meditation</h3>
+                    <h6>BREATH</h6>
+                  </div>
+                </div>
+              </GridItem>
+            </GridContainer>
+            <div className={classes.description}>
+              <p>
+                An artist of considerable range, Chet Faker — the name taken by
+                Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
+                and records all of his own music, giving it a warm, intimate
+                feel with a solid groove structure.{" "}
+              </p>
+            </div>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
+                <NavPills
+                  alignCenter
+                  color="success"
+                  tabs={[
+                    {
+                      tabButton: "Beginner",
+                      tabIcon: OndemandVideoIcon,
+                      tabContent: (
+                        <GridContainer justify="center">
+                          <GridItem xs={12} sm={12} md={12}>
+                            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/gMZ1nzQJZvU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="beginner2"></iframe>
+                            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/vbVh43mTHF4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="beginner1"></iframe>
+                            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/vH11undyI2o" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="beginner3"></iframe>
+                          </GridItem>
+                        </GridContainer>
+                      )
+                    },
+                    {
+                      tabButton: "Intermediate",
+                      tabIcon: SpaIcon,
+                      tabContent: <Intermediate />
+                    },
+                    {
+                      tabButton: "Advanced",
+                      tabIcon: TrendingUpIcon,
+                      tabContent: (
+                        <GridContainer justify="center">
+                          <GridItem xs={12} sm={12} md={4}>
+                            <img
+                              alt="..."
+                              src={work4}
+                              className={navImageClasses}
+                            />
+                            <img
+                              alt="..."
+                              src={studio3}
+                              className={navImageClasses}
+                            />
+                          </GridItem>
+                          <GridItem xs={12} sm={12} md={4}>
+                            <img
+                              alt="..."
+                              src={work2}
+                              className={navImageClasses}
+                            />
+                            <img
+                              alt="..."
+                              src={work1}
+                              className={navImageClasses}
+                            />
+                            <img
+                              alt="..."
+                              src={studio1}
+                              className={navImageClasses}
+                            />
+                          </GridItem>
+                        </GridContainer>
+                      )
+                    }
+                  ]}
+                />
+              </GridItem>
+            </GridContainer>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
