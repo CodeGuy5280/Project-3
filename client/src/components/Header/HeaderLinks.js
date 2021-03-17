a00013a76791c8cb7c257627254618b6dfbf1667
Ashley Bartholomew <ashley.52389@gmail.com>
2021-03-16 20:48:52 -0600

    link to Zen page

--- client/src/components/Header/HeaderLinks.js
+++ client/src/components/Header/HeaderLinks.js
@@ -50,7 +50,11 @@ export default function HeaderLinks(props) {
             </Link>,
             <Link to={"/yoga-page"} className={classes.dropdownLink}>
             Yoga
+            </Link>,
+            <Link to={"/Zen-page"} className={classes.dropdownLink}>
+            Zen
             </Link>
+
           ]}
         />
       </ListItem>
