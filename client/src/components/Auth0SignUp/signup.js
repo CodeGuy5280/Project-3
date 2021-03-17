4e1dc07ec26a362789d421598e0b2a2766ba6e8c
Alexander Leal <alex.j.leal@live.com>
2021-03-16 20:07:53 -0600

    transfer from test folder to project 3 folder: login, logout, signup, profile.

--- client/src/components/Auth0SignUp/signup.js
+++ client/src/components/Auth0SignUp/signup.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const LoginButton = () => {
+const SignupButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
     <button
       className="btn btn-primary btn-block"
-      onClick={() => loginWithRedirect()}
+      onClick={() =>
+        loginWithRedirect({
+          screen_hint: "signup",
+        })
+      }
     >
-      Log In
+      Sign Up
     </button>
   );
 };
 
-export default LoginButton;
+export default SignupButton;
